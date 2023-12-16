a611040c6f33cd6d00b07ddb824b9842ed2de2de
྅༻ Ǭɀħ ༄༆ཉ <ozh@ozh.org>
2023-12-16 20:33:38 +0100

    Trigger plugin with a lower priority

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: 404 if not found
 Plugin URI: https://github.com/YOURLS/404-if-not-found/
 Description: Send a 404 (instead of a 302) when short URL is not found
-Version: 1.1
+Version: 1.2
 Author: Ozh
 Author URI: https://yourls.org/
 */
@@ -12,14 +12,14 @@ Author URI: https://yourls.org/
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
 
 // 'keyword' provided ('abc' in 'http://sho.rt/abc') but not found
-yourls_add_action('redirect_keyword_not_found', 'ozh_404_if_not_found');
+yourls_add_action('redirect_keyword_not_found', 'ozh_404_if_not_found', 999);
 
 // 'keyword+' provided but this isnt an existing stat page
-yourls_add_action('infos_keyword_not_found', 'ozh_404_if_not_found');
+yourls_add_action('infos_keyword_not_found', 'ozh_404_if_not_found', 999);
 
 // 'keyword' not provided: direct attempt at http://sho.rt/yourls-go.php or -infos.php
-yourls_add_action('redirect_no_keyword', 'ozh_404_if_not_found');
-yourls_add_action('infos_no_keyword', 'ozh_404_if_not_found');
+yourls_add_action('redirect_no_keyword', 'ozh_404_if_not_found', 999);
+yourls_add_action('infos_no_keyword', 'ozh_404_if_not_found', 999);
 
 // Display a default 404 not found page
 function ozh_404_if_not_found() {
